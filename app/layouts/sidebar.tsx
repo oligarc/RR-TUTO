c53e7c29b9712d276be550d7f930b55450e5b623
oligarc <oliver25399@gmail.com>
2025-02-15 12:24:04 +0100

    fc

---
+++ app/layouts/sidebar.tsx
@@ -0,0 +1,101 @@
+import { Form, Link, Outlet,NavLink,useNavigation,useSubmit } from "react-router";
+import { getContacts } from "../data";
+import type { Route } from "./+types/sidebar";
+import { useEffect } from "react";
+
+
+export async function loader({
+    request,
+  }: Route.LoaderArgs) {
+    const url = new URL(request.url);
+    const q = url.searchParams.get("q");
+    const contacts = await getContacts(q);
+    const submit = useSubmit();
+    return { contacts,q };
+  }
+
+export default function SidebarLayout({
+  loaderData,
+}: Route.ComponentProps) {
+  const { contacts,q } = loaderData;
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    const searchField = document.getElementById("q");
+    if (searchField instanceof HTMLInputElement) {
+      searchField.value = q || "";
+    }
+  }, [q]);
+
+  return (
+    <>
+      <div id="sidebar">
+        <h1>
+          <Link to="about">React Router Contacts</Link>
+        </h1>
+        <div>
+          <Form id="search-form" onChange={(event) =>
+              submit(event.currentTarget)
+            } role="search">
+            <input
+              aria-label="Search contacts"
+              defaultValue={q || ""}
+              id="q"
+              name="q"
+              placeholder="Search"
+              type="search"
+            />
+            <div
+              aria-hidden
+              hidden={true}
+              id="search-spinner"
+            />
+          </Form>
+          <Form method="post">
+            <button type="submit">New</button>
+          </Form>
+        </div>
+        <nav>
+          {contacts.length ? (
+            <ul>
+              {contacts.map((contact) => (
+                <li key={contact.id}>
+                  <NavLink
+                className={({ isActive, isPending }) =>
+                  isActive
+                    ? "active"
+                    : isPending
+                    ? "pending"
+                    : ""
+                }
+                to={`contacts/${contact.id}`}
+              >
+                    {contact.first || contact.last ? (
+                      <>
+                        {contact.first} {contact.last}
+                      </>
+                    ) : (
+                      <i>No Name</i>
+                    )}
+                    {contact.favorite ? (
+                      <span>★</span>
+                    ) : null}
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>
+              <i>No contacts</i>
+            </p>
+          )}
+        </nav>
+      </div>
+      <div className={
+          navigation.state === "loading" ? "loading" : ""
+        } id="detail">
+        <Outlet />
+      </div>
+    </>
+  );
+}
